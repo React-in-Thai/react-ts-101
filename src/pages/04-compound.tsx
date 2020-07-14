8f57f92a0bf73f5bec6064966a29f12ba0438dca
siriwatknp <siriwatkunaporn@gmail.com>
2020-07-14 12:30:05 +0700

    feat: 🎸 add compound component file

---
+++ src/pages/04-compound.tsx
@@ -0,0 +1,19 @@
+// Grid.tsx
+import React from "react";
+
+// todo : create Grid component that contain Row & Column
+
+
+// outside
+function App() {
+  return (
+    <Grid.Row>
+      <Grid.Column>
+        <div>Content 1</div>
+      </Grid.Column>
+      <Grid.Column>
+      <div>Content 2</div>
+      </Grid.Column>
+    </Grid.Row>
+  )
+}
